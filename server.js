d59baede74135bd4a0ffaa018791fbb5604f9037
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-12-09 22:20:15 -0300

    Adding docker, docker-compose, tests and handler connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 
+const TroubleshootingHandler = require('./handlers/troubleshooting');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +14,8 @@ app.get('/auth', async (request, response) => {
   const responseToSend = await dataAuth.json();
   return response.status(200).send({ msg: responseToSend });
 });
+
+app.post('/api/v1/troubleshooting', TroubleshootingHandler);
 
 app.listen(9000, () => {
   console.log('api listening on port 9000');
