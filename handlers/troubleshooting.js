e032b24650d82c1c552922a54889868870895de6
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-12-10 20:21:56 -0300

    sending data back when creating some troubleshooting ticket and generating the random code

--- handlers/troubleshooting.js
+++ handlers/troubleshooting.js
@@ -11,7 +11,18 @@ class Troubleshooting {
       const queryExec = await sequelize.query(insertIntoTroubleshootings, {
         replacements: valuesToInsert
       });
-      return response.status(200).send({ resultQuery: queryExec });
+      console.log(queryExec);
+      if (Array.isArray(queryExec)) {
+        const reply = {
+          ...queryExec[0][0],
+          ticketCode: (Math.random() * 8)
+            .toString(36)
+            .split('.')
+            .join('')
+        };
+        return response.status(200).send({ ticket: reply });
+      }
+      return response.status(204).send({ ticket: {} });
     } catch (e) {
       console.log('E:::', e);
       return response.status(500).send(false);
