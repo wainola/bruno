3e4a65301d3e7ffeda97b924b75d85034b8fc63a
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-12-12 08:49:12 -0300

    Seeding data

--- bruno_app/seeders/20181212035540-fake_users.js
+++ bruno_app/seeders/20181212035540-fake_users.js
@@ -13,42 +13,53 @@ module.exports = {
       }], {});
     */
     const promises = [
-      queryInterface.bulkInsert('users', [
-        { name: 'john', lastname: 'doe', role: 'user', age: '33' },
-        { name: 'jane', lastname: 'done', role: 'admin', age: '35' }
-      ]),
-      queryInterface.bulkInsert('troubleshootings', [
-        {
-          flight_code: Math.random()
-            .toString(36)
-            .split('.')
-            .join(),
-          username: 'wainola',
-          info: 'tickter is overpriced',
-          sate: 'PENDING'
-        },
-        {
-          flight_code: Math.random()
-            .toString(36)
-            .split('.')
-            .join(),
-          username: 'nrriquel',
-          info: 'tickter is not in the correct format',
-          sate: 'PENDING'
-        }
-      ]),
-      queryInterface.bulkInsert('tickets', [
-        {
-          number: parseInt(Math.random() * 100, 10),
-          city: 'santiago',
-          date: Date.now()
-        },
-        {
-          numner: parseInt(Math.random() * 100, 10),
-          city: 'chiloe',
-          date: Date.now()
-        }
-      ])
+      queryInterface
+        .bulkInsert('users', [
+          { name: 'john', lastname: 'doe', role: 'user', age: '33' },
+          { name: 'jane', lastname: 'done', role: 'admin', age: '35' }
+        ])
+        .then(() => console.log('success on users insertion'))
+        .catch(e => console.log('some error ocurred on users insertion', e)),
+      queryInterface
+        .bulkInsert('troubleshootings', [
+          {
+            flight_code: Math.random()
+              .toString(36)
+              .split('.')
+              .join(),
+            username: 'wainola',
+            info: 'tickter is overpriced',
+            state: 'PENDING'
+          },
+          {
+            flight_code: Math.random()
+              .toString(36)
+              .split('.')
+              .join(),
+            username: 'nrriquel',
+            info: 'tickter is not in the correct format',
+            state: 'PENDING'
+          }
+        ])
+        .then(() => console.log('success on inserting troubleshootings data'))
+        .catch(e =>
+          console.log('some error ocurred on troubleshootings insertion', e)
+        ),
+      queryInterface
+        .bulkInsert('tickets', [
+          {
+            number: parseInt(Math.random() * 100, 10),
+            city: 'santiago',
+            date: Date.now()
+          },
+          {
+            number: parseInt(Math.random() * 100, 10),
+            city: 'chiloe',
+            date: Date.now()
+          }
+        ])
+        .then(() => console.log('success on inserting tickets data'))
+        .catch(e => console.log('some error ocurred on ticket insertion', e))
     ];
 
     return Promise.all(promises);
@@ -63,9 +74,18 @@ module.exports = {
       return queryInterface.bulkDelete('People', null, {});
     */
     const promises = [
-      queryInterface.bulkDelete('users', null, {}),
-      queryInterface.bulkDelete('tickets', null, {}),
-      queryInterface.bulkDelete('troubleshootings', null, {})
+      queryInterface
+        .bulkDelete('users', null, {})
+        .then(() => console.log('success on deleting seeded data'))
+        .catch(e => console.log('some error ocurred on deletion')),
+      queryInterface
+        .bulkDelete('tickets', null, {})
+        .then(() => console.log('success on deleting seeded data'))
+        .catch(e => console.log('some error ocurred on deletion')),
+      queryInterface
+        .bulkDelete('troubleshootings', null, {})
+        .then(() => console.log('success on deleting seeded data'))
+        .catch(e => console.log('some error ocurred on deletion'))
     ];
 
     return Promise.all(promises);
